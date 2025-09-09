72c56ee8f48ed71d56beb7a83c5ffda7b024985b
jhbai <brianbai0101@gmail.com>
2025-09-09 23:26:31 +0800

    prefill inference(input_ids and attn_mask) fix

--- src/main/python/engine/prefill.py
+++ src/main/python/engine/prefill.py
@@ -26,18 +26,20 @@ def infer(model: Gemma3ForConditionalGeneration,
             else:
                 curr_seq_len = _input_ids.shape[1]
             diff_seq_len = seq_len - curr_seq_len
-            attn_mask + [[1]*curr_seq_len + [0]*diff_seq_len]
+            attn_mask += [[1]*curr_seq_len + [0]*diff_seq_len]
+        """[TODO] 這邊的attn_mask應該要改成動態的，要跟隨cache的長度變動。例如有的task是剛建立cache，有的是已經有cache了"""
 
         # ----- Prefilling過程 ----- #
         with torch.no_grad():
-            model(input_ids=torch.FloatTensor(_input_ids).to(model.device),
+            model(
+                input_ids=torch.LongTensor(_input_ids).to(model.device),
                 use_cache=True,
                 past_key_values=cache,
                 attention_mask=torch.LongTensor(attn_mask).to(model.device)
                 )
         
         # ----- 拆解cache ----- #
-        caches = cache_manager(cache)
+        caches = cache_manager.KVCache_split(cache)
     finally:
         for item in ("input_ids", ):
             exec(f"del {item}")
